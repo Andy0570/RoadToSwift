160405a812f5d25bfb074fdb1e91e7b6d37036d3
Qilin Hu <andywhm@163.com>
2020-11-21 14:19:14 +0800

    iOS 编程，05-ProgrammaticViews

---
+++ iOS 编程（第7版）/Demo/05-ProgrammaticViews/WorldTrotter/WorldTrotter/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  WorldTrotter
+//
+//  Created by Qilin Hu on 2020/11/12.
+//
+//  主要内容：
+//  1.通过代码方式创建 Map 视图；
+//  2.通过代码方式添加约束；
+//  3.添加 UISegmentedControl 分段控件;
+//  4.将 target-action 附加到分段控制上。
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
