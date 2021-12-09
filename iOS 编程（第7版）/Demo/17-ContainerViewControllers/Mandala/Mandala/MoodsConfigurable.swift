729a78ee255c3faecb8e065da0f28e7dd5be5965
Qilin Hu <andywhm@163.com>
2021-12-09 10:45:35 +0800

    :construction: add demo: container view controller

---
+++ iOS 编程（第7版）/Demo/17-ContainerViewControllers/Mandala/Mandala/MoodsConfigurable.swift
@@ -0,0 +1,12 @@
+//
+//  MoodsConfigurable.swift
+//  Mandala
+//
+//  Created by Qilin Hu on 2021/12/9.
+//
+
+import Foundation
+
+protocol MoodsConfigurable {
+    func add(_ moodEntry: MoodEntry)
+}
---
+++ iOS 编程（第7版）/Demo/17-ContainerViewControllers/Mandala/Mandala/UIColor+Mandala.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Mandala.swift
+//  Mandala
+//
+//  Created by Qilin Hu on 2021/12/8.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let angry = UIColor(named: "angryRed")!
+    static let confused = UIColor(named: "confusedPurple")!
+    static let crying = UIColor(named: "cryingLightBlue")!
+    static let goofy = UIColor(named: "goofyOrange")!
+    static let happy = UIColor(named: "happyTurquoise")!
+    static let meh = UIColor(named: "mehGray")!
+    static let sad = UIColor(named: "sadBlue")!
+    static let sleepy = UIColor(named: "sleepyLightRed")!
+}
